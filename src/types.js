84ccebbe56b336ff2f5e847fcb2d03beea73476f
mac1228 <mac1228@gmail.com>
2018-08-05 13:31:05 -0700

    Use graphql relay connections

--- src/types.js
+++ src/types.js
@@ -12,6 +12,10 @@ import {
 import * as tables from './tables';
 import * as loaders from './loaders';
 
+import {
+  connectionDefinitions
+} from 'graphql-relay';
+
 export const NodeInterface = new GraphQLInterfaceType({
   name: 'Node',
   fields: {
@@ -30,50 +34,6 @@ export const NodeInterface = new GraphQLInterfaceType({
 const resolveId = (source) => {
   return tables.dbIdToNodeId(source.id, source.__tableName);
 };
-
-const PageInfoType = new GraphQLObjectType({
-  name: 'PageInfo',
-  fields: {
-    hasNextPage: {
-      type: new GraphQLNonNull(GraphQLBoolean)
-    },
-    hasPreviousPage: {
-      type: new GraphQLNonNull(GraphQLBoolean)
-    },
-    startCursor: {
-      type: GraphQLString,
-    },
-    endCursor: {
-      type: GraphQLString,
-    }
-  }
-});
-
-const PostEdgeType = new GraphQLObjectType({
-  name: 'PostEdge',
-  fields: () => {
-    return {
-      cursor: {
-        type: new GraphQLNonNull(GraphQLString)
-      },
-      node: {
-        type: new GraphQLNonNull(PostType)
-      }
-    }
-  }
-});
-
-const PostsConnectionType = new GraphQLObjectType({
-  name: 'PostsConnection',
-  fields: {
-    pageInfo: {
-      type: new GraphQLNonNull(PageInfoType)
-    },
-    edges: {
-      type: new GraphQLList(PostEdgeType)
-    }
-  }
-});
 
 export const UserType = new GraphQLObjectType({
   name: 'User',
@@ -152,3 +112,5 @@ export const PostType = new GraphQLObjectType({
     }
   }
 });
+
+const { connectionType: PostsConnectionType } = connectionDefinitions({ nodeType: PostType });
